867be71c5f3cf78bbbba79e5ff09fbc58b88937d
Damián Varga <damian.varga13@gmail.com>
2026-05-05 13:36:32 +0200

    comment

--- src/preprocess.py
+++ src/preprocess.py
@@ -15,6 +15,7 @@ def split_data(df, target):
     X = df.drop(columns=[target])
     y = df[target]
 
+    # If you want to create the same output every time, you can set a random_state parameter here
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, stratify=y
     )
